89d672f4423a0bebd6cbd0b7dc12e44b963486d5
s_kupr <semargal@gmail.com>
2014-08-28 15:17:58 +0300

    Fixed ResearchAreaPreview plugin

--- apps/itcq/plugins/researchareapreview/forms.py
+++ apps/itcq/plugins/researchareapreview/forms.py
@@ -1,4 +1,5 @@
 from django.forms.models import ModelForm
+from cms.models import Page
 # from cms.plugins.text.models import Text
 from itcq.plugins.researchareapreview.models import ResearchAreaPreview
 from django import forms
@@ -8,3 +9,7 @@ class ResearchAreaPreviewForm(ModelForm):
     class Meta:
         model = ResearchAreaPreview
         exclude = ('page', 'position', 'placeholder', 'language', 'plugin_type')
+
+    def __init__(self, *args, **kwargs):
+        super(ResearchAreaPreviewForm, self).__init__(*args, **kwargs)
+        self.fields['more_link'].queryset = Page.objects.drafts()
